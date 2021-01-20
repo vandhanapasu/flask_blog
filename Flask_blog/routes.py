a37e42a9a8627c1df5838681cd4f361802b618be
Vandhana Pasupathi <vandhanapasu@gmail.com>
2021-01-19 23:05:20 -0800

    Added login required logic to view account page only when they are logged in

--- Flask_blog/routes.py
+++ Flask_blog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from Flask_blog import app, db, bcrypt
 from Flask_blog.forms import RegistrationForm, LoginForm
 from Flask_blog.models import User, Post
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -68,5 +68,6 @@ def logout():
 
 
 @app.route("/account")
+@login_required
 def account():
     return render_template('account.html', title='Account')
