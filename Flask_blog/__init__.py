6e9b19d5e6bd92b45cb1587d3e672d957744dd7a
Vandhana Pasupathi <vandhanapasu@gmail.com>
2021-01-31 11:20:16 -0800

    Stage6: Created a config class for all the config values needed for the application and moved to env values

--- Flask_blog/__init__.py
+++ Flask_blog/__init__.py
@@ -1,25 +1,19 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from Flask_blog.config import Config
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '1c8b9ca15777561f1c757f1f722e41e5'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site:db'
-
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+
 mail = Mail(app)
 
 
